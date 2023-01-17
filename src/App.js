e0ded994064bd42dfb0ca09256d99fe69833949e
MELLOUK Ayoub <52928637+ayoubME@users.noreply.github.com>
2023-01-17 15:47:57 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles.css'
+
+import { CssBaseline, Container, Typography, Box } from '@mui/material'
+import SignUp from './Form'
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <SignUp />
+    </React.Fragment>
+  )
+}
